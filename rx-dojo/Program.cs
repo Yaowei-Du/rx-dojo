9af2f32846a15cdbb828772f89a4ab084fd5d4d3
Du Yaowei <d.yaowei@gmail.com>
2020-03-16 12:15:01 +0800

    fetch 20 message and print to console with rx.

--- rx-dojo/Program.cs
+++ rx-dojo/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
 
 namespace rx_dojo
 {
@@ -6,7 +8,43 @@ namespace rx_dojo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var queue = new Queue<ExampleClass>();
+
+            for (var i = 0; i < 1000; i++)
+            {
+                queue.Enqueue(new ExampleClass(i.ToString()));
+            }
+
+            var messages = getMessages(queue, 20);
+            messages.ToObservable()
+                .Subscribe(ProcessMessage);
+        }
+
+        static IEnumerable<ExampleClass> getMessages(Queue<ExampleClass> queue, int count)
+        {
+            var objs = new List<ExampleClass>();
+            for (var i = 0; i < count; i++)
+            {
+                objs.Add(queue.Dequeue());
+            }
+
+            return objs;
+        }
+
+        private static void ProcessMessage(ExampleClass obj)
+        {
+            Console.WriteLine(obj.ObjName);
+        }
+    }
+
+
+    class ExampleClass
+    {
+        public string ObjName { get; set; }
+
+        public ExampleClass(string objName)
+        {
+            this.ObjName = objName;
         }
     }
 }
