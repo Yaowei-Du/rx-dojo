2c9165e16decdf1e0a09697501ff01b257f87447
Du Yaowei <d.yaowei@gmail.com>
2020-03-16 15:42:56 +0800

    add observeOn to different scheduler.

--- rx-dojo/Program.cs
+++ rx-dojo/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Reactive.Concurrency;
 using System.Reactive.Linq;
+using System.Threading;
 
 namespace rx_dojo
 {
@@ -10,14 +12,17 @@ namespace rx_dojo
         {
             var queue = new Queue<ExampleClass>();
 
-            for (var i = 0; i < 1000; i++)
+            for (var i = 1; i < 1000; i++)
             {
                 queue.Enqueue(new ExampleClass(i.ToString()));
             }
 
-            var messages = getMessages(queue, 20);
-            messages.ToObservable()
-                .Subscribe(ProcessMessage);
+            getMessages(queue, 10).ToObservable()
+                .Select(ProcessMessage)
+                .ObserveOn(NewThreadScheduler.Default)
+                .Select(ProcessMessage2)
+                .ObserveOn(NewThreadScheduler.Default)
+                .Subscribe(ProcessMessage3);
         }
 
         static IEnumerable<ExampleClass> getMessages(Queue<ExampleClass> queue, int count)
@@ -31,12 +36,30 @@ namespace rx_dojo
             return objs;
         }
 
-        private static void ProcessMessage(ExampleClass obj)
+        private static ExampleClass ProcessMessage(ExampleClass obj)
         {
-            Console.WriteLine(obj.ObjName);
+            Console.WriteLine($"Message: {obj.ObjName}, in Method1#begin, Thread: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(new Random().Next(10) * 100);
+            Console.WriteLine($"Message: {obj.ObjName}, in Method1#end, Thread: {Thread.CurrentThread.ManagedThreadId}");
+            return obj;
+        }
+
+
+        private static ExampleClass ProcessMessage2(ExampleClass obj)
+        {
+            Console.WriteLine($"Message: {obj.ObjName}, in Method2#begin, Thread: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(new Random().Next(10) * 100);
+            Console.WriteLine($"Message: {obj.ObjName}, in Method2#end, Thread: {Thread.CurrentThread.ManagedThreadId}");
+            return obj;
+        }
+
+        private static void ProcessMessage3(ExampleClass obj)
+        {  
+            Console.WriteLine($"Message: {obj.ObjName}, in Method3#begin, Thread: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(new Random().Next(10) * 100);
+            Console.WriteLine($"Message: {obj.ObjName}, in Method3#end, Thread: {Thread.CurrentThread.ManagedThreadId}");
         }
     }
-
 
     class ExampleClass
     {
